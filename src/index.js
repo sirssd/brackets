d151054dd4d76906ca09270152f84245e7612c69
Sergey Durov <sergssd@yandex.com>
2020-10-07 16:37:36 +0500

    feat: add my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketsConfig) { // check ( '([{}])' ,  [ [ '(' ,  ')' ] ,  [ '[' ,  '] ' ],  [ '{' ,  '}' ] ] )
+    let arrFromStr = str.trim().split(' ');
+    let brackets = [];
+    arrFromStr = arrFromStr.join('').split('');
+
+    while (arrFromStr.length != 0) {
+        let count = 0;
+        bracketsConfig.forEach(item => {
+            for (let i = 0; i < arrFromStr.length; i++) {
+                item = item.join('').trim();
+                item = item.split(' ').join('').split('');
+                if (item.join('') == arrFromStr.slice(i, i + 2).join('').trim()) {
+                    brackets.push(true);
+                    return arrFromStr.splice(i, 2);
+                }
+            }
+            count++;
+        });
+        if (arrFromStr.length == 1 || count == bracketsConfig.length) return false;
+    }
+
+    return Array.isArray(brackets);
 }
